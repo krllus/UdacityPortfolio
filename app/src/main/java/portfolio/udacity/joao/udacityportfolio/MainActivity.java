7953ab325e6b0dd2ec1e91245d54cb12c5d5cc9c
Joao Carlos Ferreira Marques <joaocarlusferrera@gmail.com>
2015-06-03 14:57:46 -0500

    First Commit
    TODO
        - Organize main.xml
        - styles of buttons
        - string.xml for buttons text + tag

---
+++ app/src/main/java/portfolio/udacity/joao/udacityportfolio/MainActivity.java
@@ -0,0 +1,47 @@
+package portfolio.udacity.joao.udacityportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //stack overflow
+    //http://stackoverflow.com/questions/5706942/possibility-to-add-parameters-in-button-xml
+    public void callToast(View v) {
+        String tag = v.getTag().toString();
+        Toast.makeText(MainActivity.this, "this button will launch my " + tag, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
